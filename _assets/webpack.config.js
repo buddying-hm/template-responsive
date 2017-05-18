9f03c82b5270d1cba61fadc01fdabd256c3355ad
Naoyuki Miyazaki <miyazaki@buddying.jp>
2017-05-18 17:36:39 +0900

    venderじゃなくてvendorらしい...

--- _assets/webpack.config.js
+++ _assets/webpack.config.js
@@ -29,7 +29,7 @@ const webpackConf = [
         jQuery: 'jquery',
       }),
       new CopyWebpackPlugin([
-        { from: `${p_path._assets}/js/vender/**/*.js`, to: p_path.output.js, flatten: true },
+        { from: `${p_path._assets}/js/vendor/**/*.js`, to: p_path.output.js, flatten: true },
         { from: `${p_path._assets}/img`, to: p_path.output.img }
       ]),
     ],
