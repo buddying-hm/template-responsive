ce7c2861e06a5c15bbf8a6d4a7e9316f5ca096d5
NaoyukiMiyazaki <miyazaki@buddying.jp>
2017-05-29 18:30:54 +0900

    Update image-trimming.js

--- _assets/js/libs/image-trimming.js
+++ _assets/js/libs/image-trimming.js
@@ -3,31 +3,32 @@
  * 依存: jquery
  */
 
-export default function imageTrimming($class_name) {
-    $(window).on('load resize', function(){
-            trimming($class_name);
-    })
-    function trimming ($photoBox) {
-        jQuery($photoBox).each(function(){
-            var box = jQuery(this);
-            var i = jQuery('img',this);
-            var box_w =$(box).width();
-            var box_h =$(box).height();
-            var i_w =$(i).width();
-            var i_h =$(i).height();
-            var i_par =i_w / i_h; //画像の縦横比
-            var box_par =box_w / box_h;
-            if (i_par > box_par) { //画像が枠より横長の場合高さ100%で幅左右を切る
-                $(i).css({
-                    "width": "auto",
-                    "height":"100%"
-                });
-            }else{
-                $(i).css({//画像が枠より縦長または同じの場合幅100%にして高さの上下を切る
-                    "width": "100%",
-                    "height":"auto"
-                });
-            }
-        });
+export default function imageTrimming(wrapperSelector) {
+  $(wrapperSelector).each(function() {
+    var $box = $(this);
+    var $img = $('img', this);
+    var img_par = getAspectRatio($box); 
+    var box_par = getAspectRatio($img);
+    if (img_par > box_par) {
+      //画像が枠より横長の場合高さ100%で幅左右を切る
+      $img.css({
+        width: 'auto',
+        height: '100%'
+      });
+    } else {
+      //画像が枠より縦長または同じの場合幅100%にして高さの上下を切る
+      $img.css({
+        width: '100%',
+        height: 'auto'
+      });
     }
+  });
+}
+
+/**
+ * 画像の縦横比取得関数
+ * @param {jQueryElement} $elem
+ */
+function getAspectRatio($elem) {
+  return $elem.width() / $elem.height();
 }
