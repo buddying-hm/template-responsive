822b2d1993f65a5e4157d7e43ce48592c25690fb
NaoyukiMiyazaki <miyazaki@buddying.jp>
2017-05-29 15:49:17 +0900

    Update smoothscroll.js

--- _assets/js/libs/smoothscroll.js
+++ _assets/js/libs/smoothscroll.js
@@ -4,12 +4,13 @@
  * スクロールの速度: ミリ秒
  */
 export default function smoothScroll(param) {
-  $('a[href^="#"]').on('click', e => {
-    const scrollSpeed = param.speed || 400
-    const href= $(e.target).attr('href');
-    const target = $(href === '#' || href === '' ? 'html' : href)
-    const position = target.offset().top
-    $('body,html').animate({scrollTop:position}, scrollSpeed, 'swing')
-    return
+  const scrollSpeed = param.speed || 400;
+  
+  $('a[href^="#"]').on('click', function() {
+    const href= $(this).attr('href');
+    const target = $(href === '#' || href === '' ? 'html' : href);
+    const position = target.offset().top;
+    $('body,html').animate({scrollTop:position}, scrollSpeed, 'swing');
+    return;
   })
 }
