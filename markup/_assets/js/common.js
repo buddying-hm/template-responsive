85d68e3533c013147aad1adfcf8fd8a193c992ef
Haruka Munekata <munekata@buddying.jp>
2017-07-18 12:58:26 +0900

    Update common.js

--- markup/_assets/js/common.js
+++ markup/_assets/js/common.js
@@ -1,16 +1,4 @@
-import preventPcTelLink from './libs/prevent-pc-tel-link';
-import smoothscroll from './libs/smoothscroll';
-import FitElem from './libs/fit-elem';
-
-/**
- * main function
- * @returns {undefined}
- */
 function main() {
-  smoothscroll(400);
-  preventPcTelLink();
-  new FitElem('.box', 'img');
-
   console.log('hi');
 }
 
